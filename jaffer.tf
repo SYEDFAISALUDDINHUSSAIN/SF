667773c5a63677abad8343a016266e85d7609f8f
SYEDFAISALUDDINHUSSAIN <83583282+SYEDFAISALUDDINHUSSAIN@users.noreply.github.com>
2021-05-04 16:54:06 +0530

    Update jaffer.tf

--- jaffer.tf
+++ jaffer.tf
@@ -1,15 +1,9 @@
 provider "aws" {
 region = "ca-central-1"
-access_key = ""
-secret_key = ""
-}
-resource "aws_instance" "WINDOWS2" {
-ami = "ami-0953812634687f0c6"
-instance_type = "t2.micro"
-count = "2"
-key_name = "windowspassword"
+access_key = "AKIAX4EFVBOR2GEVKXLI"
+secret_key = "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"
 }
 resource "aws_s3_bucket" "ABC" {
-bucket = "mj970"
+bucket = "faisalbucket40"
 acl = "private"
 }
